356d9188a7cda3d4804d6cfaef90e1199cd93d9c
Jeremy Wipperfurth <Jerwip3@Gmail.com>
2024-02-29 22:13:02 -0600

    Added secret message

--- site.js
+++ site.js
@@ -1,3 +1,12 @@
+// You are going to send me a "secret" message, using localStorage. Modify your site.js file so that when it runs, it adds a message to localStorage. The message should be a string, and it can be anything you want. It could be a simple message, or a joke, or a quote, or anything else you want me to see.
+
+// You must use the key It's a secret to everybody.. It must be spelled exactly like that, with the same capitalization and punctuation. If not, my code won't be able to find it. If I can't find it, you'll get a zero score, and a comment that says, "Message not received. Try again."
+
+// 500 brownie points if you your message is a reference to something related to that key.
+
+
+
+
 const hours = new Date().getHours() // get the current hour
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
@@ -9,10 +18,8 @@ const welcomeDiv = document.querySelector('#welcome')
 const pageTitle = document.title
 
 // sets the welcome message based on the time of day
-if (isMorning) {
-    welcomeDiv.textContent = `Good morning! Welcome to ${pageTitle}.`
-} else if (isAfternoon) {
-    welcomeDiv.textContent = `Good afternoon! Welcome to ${pageTitle}.`
-} else {
-    welcomeDiv.textContent = `Good evening! Welcome to ${pageTitle}.`
-}
+if (isMorning) welcomeDiv.textContent = `Good morning! Welcome to ${pageTitle}.`
+else if (isAfternoon) welcomeDiv.textContent = `Good afternoon! Welcome to ${pageTitle}.`
+else welcomeDiv.textContent = `Good evening! Welcome to ${pageTitle}.`
+
+localStorage.setItem("It's a secret to everybody.", "It's dangerous to go alone!")
